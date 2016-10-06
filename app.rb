c2c245affb2572c8848027f0872a4c8fb34840b5
Dirty Girty <spam@dirtygirty.com>
2016-10-06 17:12:58 +0000

    added some routes for retrieving logs, restarting the app and default git path.

--- app.rb
+++ app.rb
@@ -1,7 +1,35 @@
 require 'sinatra'
+require 'open3'
 
 class TheApp < Sinatra::Base
   get '/' do
-    "<h1>Hello, world!</h1>"
+    code = params[:code].to_i || 200
+    sleep(params[:sleep].to_i) if params[:sleep]
+    halt code, "#{params}"
   end
+
+  get '/app_logs' do
+    File.read(LOGFILE)
+  end
+
+  get '/apache_access_logs' do
+    File.read('/var/log/apache2/access.log')
+  end
+
+  get '/apache_error_logs' do
+    File.read('/var/log/apache2/error.log')
+  end
+
+  get '/restart' do
+    msg = ""
+    code = 200
+    Open3.popen2e("touch #{APP_DIR}/tmp/restart.txt") {|stdin, stdout_and_stderr, wait_thr|
+      pid = wait_thr.pid # pid of the started process.
+      exit_status = wait_thr.value # Process::Status object returned.
+      msg = "#{stdout_and_stderr.read}: #{exit_status}" 
+      code = 500 if exit_status != 0
+    }
+    halt code, msg 
+  end
+
 end
--- config.ru
+++ config.ru
@@ -1,5 +1,14 @@
 require 'rubygems'
 require 'sinatra'
+require 'logger'
 require File.expand_path '../app.rb', __FILE__
+
+APP_DIR = Dir.pwd
+LOGFILE = File.join(APP_DIR, "dirtygirty.log")
+configure do
+  LOGGER = Logger.new(LOGFILE)
+  enable :logging, :dump_errors
+  set :raise_errors, true
+end
 
 run TheApp
